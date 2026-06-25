84ff0c5ce88aac9905f8aba0558152e7b1133c60
nashrahjaan53-code <nashrahjaan53@gmail.com>
2026-06-25 15:43:24 +0530

    converted it into python on leet did in c++

--- count-subarrays.py
+++ count-subarrays.py
@@ -1,20 +1,17 @@
+from typing import List
+
 class Solution:
     def countSubarrays(self, nums: List[int], target: int) -> int:
         n = len(nums)
         count = 0
         
-        # Check every subarray
         for i in range(n):
             target_count = 0
             for j in range(i, n):
-                # Count occurrences of target in current subarray
                 if nums[j] == target:
                     target_count += 1
                 
-                # Length of current subarray
                 length = j - i + 1
-                
-                # Check if target appears more than half
                 if target_count > length / 2:
                     count += 1
         
