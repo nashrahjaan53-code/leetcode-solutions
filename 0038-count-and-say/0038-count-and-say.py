ad4f3e501501cfaaf5ad3e4f7e191388c27b3ec8
Nashrah Khan <nashrahjaan53@gmail.com>
2026-06-26 23:19:08 +0530

    Time: 15 ms (16.43%), Space: 12.3 MB (89.54%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countAndSay(self, n):
+        result = "1"
+
+        for _ in range (1, n):
+            next_result = ""
+            i = 0
+
+            while i < len(result):
+                count = 1
+                while i + 1 < len(result) and result[i] == result[i + 1]:
+                    count += 1
+                    i += 1
+                next_result += str(count) + result[i]
+                i += 1
+            result = next_result
+
+        return result        
+        
+
+  
+        
