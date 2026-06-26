2e8a4374adf72fb7b20dddc6f2efa42d5445e308
Nashrah Khan <nashrahjaan53@gmail.com>
2026-06-26 14:55:56 +0530

    Time: 0 ms (100%), Space: 12.4 MB (69.11%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isValid(self, s):
+        stack = []
+        bracket_map = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        for char in s:
+            if char in bracket_map:
+                top_element =stack.pop() if stack else '#'
+
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack         
+
+
+
+
+
+
+
+
+
+
+        
