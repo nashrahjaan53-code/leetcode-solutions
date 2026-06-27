70d7d82355e6df20a5dd420f620f665697ca4879
Nashrah Khan <nashrahjaan53@gmail.com>
2026-06-27 22:44:40 +0530

    Time: 0 ms (100%), Space: 12.3 MB (92.09%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,23 @@
+class Solution:
+    def uniquePaths(self, m, n):
+ 
+        dp = [1] * n
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[j] += dp[j - 1]
+        
+        return dp[n - 1]
+
+
+
+ 
+
+               
+
+        
+
+
+
+     
+        
