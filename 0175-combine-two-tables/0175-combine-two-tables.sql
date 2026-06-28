98292f152ab848e17a92d3c7200dd6c0a73b5707
Nashrah Khan <nashrahjaan53@gmail.com>
2026-06-28 19:09:10 +0530

    Time: 400 ms (90.29%), Space: 0B (100%) - LeetHub

---
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -0,0 +1,12 @@
+# Write your MySQSELECT 
+    select p.firstName, p.lastName, a.city,a.state
+     From Person p left join Address a on P.PersonId=a.personId;
+ 
+ 
+
+
+
+
+
+   
+                   
