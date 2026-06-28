7794fae81a5dd6653252a50a218ca9d76f4d366a
Nashrah Khan <nashrahjaan53@gmail.com>
2026-06-28 12:07:14 +0530

    Time: 31 ms (94.74%), Space: 20.8 MB (10.53%) - LeetHub

---
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr):
+        arr.sort()
+        arr[0] = 1
+        for i in range(1, len(arr)):
+            if arr[i] - arr[i - 1] > 1:
+                arr[i] = arr[i - 1] + 1
+        return arr[-1]
+        
+       
+        
+       
+        
