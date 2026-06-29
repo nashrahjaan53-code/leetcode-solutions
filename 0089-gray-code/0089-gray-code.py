6b8494420b4d3d35f469d6ef59148cc8cc5e4f06
Nashrah Khan <nashrahjaan53@gmail.com>
2026-06-29 22:06:46 +0530

    Time: 11 ms (41.88%), Space: 20.7 MB (62.15%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.py
@@ -0,0 +1,13 @@
+class Solution:
+    def grayCode(self, n):
+        result = []
+        for i in range(1 << n):
+            result.append(i ^ (i >> 1))
+        return result
+
+
+
+
+
+
+        
