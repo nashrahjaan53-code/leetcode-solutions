76efc0cb6954c43fd872de8edc0e7e885ce98504
Nashrah Khan <nashrahjaan53@gmail.com>
2026-07-01 20:38:31 +0530

    Time: 1 ms (23.67%), Space: 12.6 MB (98.19%) - LeetHub

---
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def levelOrderBottom(self, root):
+        if not root:
+            return []
+        result = []
+        queue = [root]
+
+        while queue:
+            level_size = len(queue)
+            current_level = []
+
+            for _ in range(level_size):
+                node = queue.pop(0)
+                current_level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(current_level)
+        return result[:: -1]
+        
+
+
+
+
+
+
+
+
+
+
+
+        
