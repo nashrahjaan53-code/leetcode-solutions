0c669f6b0b9021d7769520b73057035d118e1bc3
Nashrah Khan <nashrahjaan53@gmail.com>
2026-07-02 13:33:06 +0530

    Time: 0 ms (100%), Space: 12.4 MB (52.48%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -0,0 +1,25 @@
+class Solution:
+    def largestAltitude(self, gain):
+        max_altitude = 0
+        current_altitude = 0
+        
+        for g in gain:
+            current_altitude += g
+            max_altitude = max(max_altitude, current_altitude)
+        
+        return max_altitude
+
+
+
+        
+
+
+       
+        
+
+
+
+
+
+
+        
