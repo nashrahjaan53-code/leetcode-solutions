b6de639f623fbbfba7510de7836bba5adda9753c
Nashrah Khan <nashrahjaan53@gmail.com>
2026-07-20 12:26:31 +0530

    Time: 3 ms (81.05%), Space: 12.6 MB (80%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def shiftGrid(self, grid, k):
+        m, n = len(grid), len(grid[0])
+        flat = sum(grid, [])
+        k = k % (m * n)
+        flat = flat[-k:] + flat[:-k] if k else flat
+        return [flat[i*n:(i+1)*n] for i in range(m)]
+
+
+
+
+
