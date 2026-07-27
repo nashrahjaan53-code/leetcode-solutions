f074883336fdddaa01e3b1002e9b33257be6e129
Nashrah Khan <nashrahjaan53@gmail.com>
2026-07-27 11:41:38 +0530

    Time: 0 ms (100%), Space: 12.2 MB (97.27%) - LeetHub

---
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProduct(self, nums):
+        largest = second_largest = 0
+        for num in nums:
+            if num > largest:
+                second_largest = largest
+                largest = num
+            elif num > second_largest:
+                second_largest = num
+        return(largest - 1) * (second_largest - 1)
+
+
+
+
+
+        
