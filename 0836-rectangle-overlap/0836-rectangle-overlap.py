442075d9e5523f2d99cd3e72b4e85acfa106a028
Nashrah Khan <nashrahjaan53@gmail.com>
2026-09-14 10:00:10 +0530

    Time: 0 ms (100%), Space: 12.3 MB (53.89%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isRectangleOverlap(self, rec1, rec2):
+        return not (rec1[2] <= rec2[0] or 
+                    rec2[2] <= rec1[0] or  
+                    rec1[3] <= rec2[1] or  
+                    rec2[3] <= rec1[1]) 
+       
+
+                                  
+
+
+
+
+        
